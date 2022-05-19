49439ab1b9c7ba05481f72b47b3b2c332c2a8d49
johnny19941216 <wuzhiqiang94@hotmail.com>
2022-05-19 19:48:02 +0800

    fix: 修复env错误

--- config.ts
+++ config.ts
@@ -1,7 +1,5 @@
 const webhookKey = Deno?.env?.get("WEBHOOK_KEY") ?? "";
 
-console.log(webhookKey);
-
 export const webhook = `https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=${webhookKey}`;
 
 export const maxSize = 2 * 1024 * 1024;
