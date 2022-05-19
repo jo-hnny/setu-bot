01fb6d4973cc2c535ea20a87bf66f7549d869029
johnny19941216 <wuzhiqiang94@hotmail.com>
2022-05-19 20:25:25 +0800

    fix: 建议使用准确的版本

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
-export * as base64 from "https://deno.land/std/encoding/base64.ts";
+export * as base64 from "https://deno.land/std@0.140.0/encoding/base64.ts";
 
-export { Md5 } from "https://deno.land/std/hash/md5.ts";
+export { Md5 } from "https://deno.land/std@0.140.0/hash/md5.ts";
 
-export { randomItem } from "https://deno.land/x/random_item/mod.ts";
+export { randomItem } from "https://deno.land/x/random_item@v1.2.0/mod.ts";
