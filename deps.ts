ac3d7dd3bac139003dca3eed459085a831fb06c8
johnny19941216 <wuzhiqiang94@hotmail.com>
2022-05-19 20:23:28 +0800

    feat: update deps version

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
 export * as base64 from "https://deno.land/std/encoding/base64.ts";
 
-export { Md5 } from "https://deno.land/std@0.138.0/hash/md5.ts";
+export { Md5 } from "https://deno.land/std/hash/md5.ts";
 
 export { randomItem } from "https://deno.land/x/random_item/mod.ts";
